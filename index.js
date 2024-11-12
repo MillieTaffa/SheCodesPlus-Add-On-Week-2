a30d76508a14d11151f0a5a6c298060e1d31b99e
MillieTaffa <161293050+MillieTaffa@users.noreply.github.com>
2024-11-12 16:11:30 +0200

    I added a new line of code(html) and added a new function(JS)

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
-// Los Angeles
+// Update the time for cities (like Los Angeles and Paris)
 function updateTime() {
     let losangeles = document.querySelector("#los-angeles");
-    let losangelesDate = losangeles.querySelector(".date");
-    let losangelesTime = losangeles.querySelector(".time");
-    let currentTimeLA = moment().tz("America/Los_Angeles");
-    losangelesDate.innerHTML = currentTimeLA.format("MMMM Do YYYY");
-    losangelesTime.innerHTML = `${currentTimeLA.format('h:mm:ss')} 
-    <small>${currentTimeLA.format("A")}</small>`;
+    if (losangeles) {
+        let losangelesDate = losangeles.querySelector(".date");
+        let losangelesTime = losangeles.querySelector(".time");
+        let currentTimeLA = moment().tz("America/Los_Angeles");
+        losangelesDate.innerHTML = currentTimeLA.format("MMMM Do YYYY");
+        losangelesTime.innerHTML = `${currentTimeLA.format('h:mm:ss')} <small>${currentTimeLA.format("A")}</small>`;
+    }
 
-    // Paris
     let paris = document.querySelector("#paris");
-    let parisDate = paris.querySelector(".date");
-    let parisTime = paris.querySelector(".time");
-    let currentTimeP = moment().tz("Europe/Paris");
-    parisDate.innerHTML = currentTimeP.format("MMMM Do YYYY");
-    parisTime.innerHTML = `${currentTimeP.format('h:mm:ss')} 
-    <small>${currentTimeP.format("A")}</small>`;
+    if (paris) {
+        let parisDate = paris.querySelector(".date");
+        let parisTime = paris.querySelector(".time");
+        let currentTimeP = moment().tz("Europe/Paris");
+        parisDate.innerHTML = currentTimeP.format("MMMM Do YYYY");
+        parisTime.innerHTML = `${currentTimeP.format('h:mm:ss')} <small>${currentTimeP.format("A")}</small>`;
+    }
 };
 
+// Function to update city time and display it
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess(); // Detect current time zone
+    }
+
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = ` 
-    <div>
-    <h2>${cityName}</h2>
-    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format('h:mm:ss')}  <small>${cityTime.format("A")}</small></div>`
+
+    // Check if the city already exists and update its content
+    let cityElement = document.querySelector(`#${cityName.toLowerCase()}`);
+    if (cityElement) {
+        let cityDate = cityElement.querySelector(".date");
+        let cityTimeElement = cityElement.querySelector(".time");
+        cityDate.innerHTML = cityTime.format("MMMM Do YYYY");
+        cityTimeElement.innerHTML = `${cityTime.format('h:mm:ss')} <small>${cityTime.format("A")}</small>`;
+    } else {
+        // If the city doesn't exist, create it dynamically
+        let citiesElement = document.querySelector("#cities");
+        citiesElement.innerHTML = `
+            <div class="city" id="${cityName.toLowerCase()}">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format('h:mm:ss')} <small>${cityTime.format("A")}</small></div>
+            </div>`;
+    }
 }
 
+// Initialize time updates for default cities
 updateTime();
-setInterval(updateTime, 1000)
+setInterval(updateTime, 1000); // Keep the interval running to update the time
 
+// Add event listener to the dropdown
 let citiesSelectElement = document.querySelector("#city");
-
-citiesSelectElement.addEventListener("change", updateCity)
+citiesSelectElement.addEventListener("change", updateCity);
